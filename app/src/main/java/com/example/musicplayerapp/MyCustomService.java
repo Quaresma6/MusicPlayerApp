80c225f54636b8a9458d15308b3a8b4869db3578
Quaresma6 <tomas.r.quaresma@hotmail.com>
2023-10-25 01:23:41 +0100

    first commit

---
+++ app/src/main/java/com/example/musicplayerapp/MyCustomService.java
@@ -0,0 +1,37 @@
+package com.example.musicplayerapp;
+
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.IBinder;
+import android.provider.Settings;
+
+import androidx.annotation.Nullable;
+
+public class MyCustomService extends Service {
+
+    //To play music, we need a media player object
+    private MediaPlayer player;
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        player = MediaPlayer.create(this, Settings.System.DEFAULT_RINGTONE_URI);
+        player.setLooping(true);
+        player.start();
+        return START_STICKY;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        player.stop();
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
+    }
+
+}
